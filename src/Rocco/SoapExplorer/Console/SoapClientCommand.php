e5a82819d44e4f11e5d90de28b1fd5b331cfb339
Nico Di Rocco <dirocco.nico@gmail.com>
2013-01-12 11:21:54 +0100

    Moved help text to a CONST in SoapClientCommand

--- src/Rocco/SoapExplorer/Console/SoapClientCommand.php
+++ src/Rocco/SoapExplorer/Console/SoapClientCommand.php
@@ -9,6 +9,47 @@ use Rocco\SoapExplorer\Soap\SoapClient;
 
 class SoapClientCommand extends Application
 {
+  const HELP = '
+php-cli-soap-client                                          version 1.2
+                                                           Nico Di Rocco
+
+A command line application to explore SOAP web services
+
+Usage: %s --endpoint wsdl [options] [action] [method]
+
+
+OPTIONS
+
+    -h, --help       Print this help message.
+    -q, --quiet      Surpress any kind of output. This option takes pre-
+                     cedence ofer the `-d` or `--debug` option.
+    -d, --debug      Output more verbose messages. Only works if `-q` or
+                     `--quiet` is not specified.
+    -e, --endpoint   Specify the wsdl to inspect. Alternatively you can
+                     set the environment variable SOAP_ENDPOINT.
+    -c, --cache      Flag to enable caching of the wsdl. By default this
+                     is disabled.
+    -u, --use-editor This option is only relevant when you use the `call`
+                     action. If specified the editor in EDITOR environment
+                     variable will be opened up.
+
+ACTIONS
+
+    The action to perform. If a action is omitted it defaults to `list`.
+    The following actions are supported:
+
+    list             Get a list of available methods to call on the remote.
+    wsdl             Outputs the raw wsdl in xml format.
+    request <method> Generate an xml formatted SOAP request for the given
+                     method and output to stdout.
+    call <method>    Call the remote service with the `method` specified
+                     and output the reponse to stdout.
+
+METHOD
+
+    Specify the method to call on the remote service
+
+';
   const DEFAULT_TIMEOUT = 120;
 
   protected $remote_service;
@@ -17,6 +58,8 @@ class SoapClientCommand extends Application
   protected function configure()
   {
     $this->log = new Logger();
+
+    $this->set_help(self::HELP);
 
     ini_set('default_socket_timeout', self::DEFAULT_TIMEOUT);
 
--- src/cli.php
+++ src/cli.php
@@ -2,50 +2,7 @@
 
 require_once 'autoloader.php';
 
-$HELP =
-'php-cli-soap-client                                         version 1.2
-                                                           Nico Di Rocco
-
-A command line application to explore SOAP web services
-
-Usage: %s --endpoint wsdl [options] [action] [method]
-
-
-OPTIONS
-
-    -h, --help       Print this help message.
-    -q, --quiet      Surpress any kind of output. This option takes pre-
-                     cedence ofer the `-d` or `--debug` option.
-    -d, --debug      Output more verbose messages. Only works if `-q` or
-                     `--quiet` is not specified.
-    -e, --endpoint   Specify the wsdl to inspect. Alternatively you can
-                     set the environment variable SOAP_ENDPOINT.
-    -c, --cache      Flag to enable caching of the wsdl. By default this
-                     is disabled.
-    -u, --use-editor This option is only relevant when you use the `call`
-                     action. If specified the editor in EDITOR environment
-                     variable will be opened up.
-
-ACTIONS
-
-    The action to perform. If a action is omitted it defaults to `list`.
-    The following actions are supported:
-
-    list             Get a list of available methods to call on the remote.
-    wsdl             Outputs the raw wsdl in xml format.
-    request <method> Generate an xml formatted SOAP request for the given
-                     method and output to stdout.
-    call <method>    Call the remote service with the `method` specified
-                     and output the reponse to stdout.
-
-METHOD
-
-    Specify the method to call on the remote service
-
-';
-
 $app = new Rocco\SoapExplorer\Console\SoapClientCommand($_SERVER['argv'][0]);
-$app->set_help($HELP);
 $retval = $app->bootstrap();
 
 exit($retval);
