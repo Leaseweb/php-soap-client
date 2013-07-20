f6141fb29b4fc1c247d2536543a979d5acd40975
Nico Di Rocco <dirocco.nico@gmail.com>
2013-07-20 22:29:25 +0200

    Bump version number to 2.0.0

--- src/PhpSoapClient/Application.php
+++ src/PhpSoapClient/Application.php
@@ -7,18 +7,23 @@ use Symfony\Component\Console\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
+  protected static $NAME = 'php-soap-client';
+  protected static $VERSION = '2.0.0';
+
   public function __construct()
   {
-    parent::__construct('php-soap-client', '1.3.0');
+    parent::__construct(self::$NAME, self::$VERSION);
   }
 
   protected function getDefaultCommands()
   {
     $commands = parent::getDefaultCommands();
+
     $commands[] = new \PhpSoapClient\Command\GetWsdlCommand();
     $commands[] = new \PhpSoapClient\Command\CallMethodCommand();
     $commands[] = new \PhpSoapClient\Command\GetMethodRequestXmlCommand();
     $commands[] = new \PhpSoapClient\Command\ListMethodsCommand();
+
     return $commands;
   }
 }
