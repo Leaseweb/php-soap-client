582a4fa9323b3f9bd1e82b8e2e9190835786e414
Nico Di Rocco <dirocco.nico@gmail.com>
2014-02-17 22:31:19 +0100

    Added a a TestService SoapServer class for local testing

---
+++ TestService.php
@@ -0,0 +1,54 @@
+<?php
+
+require __DIR__.'/src/bootstrap.php';
+
+ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
+
+$URI = 'http://127.0.0.1:7999/TestService.php';
+
+
+
+/**
+ * Class TestService
+ *
+ * @author Nico Di Rocco <dirocco.nico@gmail.com>
+ */
+class TestService
+{
+    /**
+     * SayHello
+     *
+     * @param string $name
+     * @return string
+     */
+    public function SayHello($name)
+    {
+        return sprintf("Hello %s", $name);
+    }
+
+    /**
+     * CalculateMD5
+     *
+     * @param string $message
+     * @return
+     */
+    public function CalculateMD5($message)
+    {
+        return array(
+            'message' => $message,
+            'md5hash' => md5($message)
+        );
+    }
+}
+
+
+if (isset($_GET['wsdl'])) {
+    $wsdl = new \WSDL\WSDLCreator('TestService', $URI);
+    $wsdl->setNamespace("http://example.com/");
+    $wsdl->renderWSDL();
+    exit;
+}
+
+$server = new SoapServer(null, array('uri' => $URI));
+$server->setClass("TestService");
+$server->handle();
