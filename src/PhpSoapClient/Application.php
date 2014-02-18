a3d4a4118ac272583ce109298d3662c5d828072a
Nico Di Rocco <dirocco.nico@gmail.com>
2014-02-18 22:50:04 +0100

    Bumped version number to 2.1.4

--- src/PhpSoapClient/Application.php
+++ src/PhpSoapClient/Application.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputOption;
 class Application extends BaseApplication
 {
     protected static $NAME = 'php-soap-client';
-    protected static $VERSION = '2.1.3';
+    protected static $VERSION = '2.1.4';
 
     public function __construct()
     {
